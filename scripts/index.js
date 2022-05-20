22df66e2340c8751af058328668d75a30b9ec803
Andrey Shebyakin <mrjayqube@yandex.ru>
2022-05-20 15:01:00 +0300

    Переименованны функции

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 const popup = document.querySelector('.popup');
-const editProfileButton = document.querySelector('.profile__edit-btn');
-const closeIconButton = document.querySelector('.popup__close-btn');
+const openPopupButton = document.querySelector('.profile__edit-btn');
+const closePopupButton = document.querySelector('.popup__close-btn');
 const formElement = document.querySelector('.popup__container');
 const nameInput = document.querySelector('.popup__item_type_name');
 const nameContent = document.querySelector('.profile__username');
 const jobInput = document.querySelector('.popup__item_type_description');
 const jobContent = document.querySelector('.profile__description');
 
-function editProfile() {
+function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = nameContent.textContent;
   jobInput.value = jobContent.textContent;
 }
 
-function closeIcon() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
@@ -21,10 +21,10 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   nameContent.textContent = nameInput.value;
   jobContent.textContent = jobInput.value;
-  closeIcon()
+  closePopup()
 }
 
 
-editProfileButton.addEventListener('click', editProfile);
-closeIconButton.addEventListener('click', closeIcon);
+openPopupButton.addEventListener('click', openPopup);
+closePopupButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
